146129f241fd077860da6217dc168c8451f6cbc7
connork96 <connorkelly96@yahoo.com>
2022-09-14 23:32:31 -0400

    updated some of the index and fixed spelling error

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# Professional-README-Generator
+# s
+  ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+## Description 📝
+
+s
+
+
+## Table of Contents 🗒
+
+* [Insttallations](#dependencies)
+
+* [Usage](#usage)
+
+
+* [License](#license)
+
+
+* [Contributors](#contributors)
+
+* [Test](#test)
+
+
+##  Insttallations (Dependencies) 💻
+
+To install dependencies, run these commands:
+
+```
+npm i
+```
+
+## Usage
+
+
+
+## License
+
+This project is licensed by undefined.
+
+## Contributors
+
+
+contact me @ 
+
+
+## Tests 🧪
+
+To run tests, run these commands:
+
+```
+npm test
+```
--- index.js
+++ index.js
@@ -1,30 +1,72 @@
-// TODO: Include packages needed for this application
+//dependencies
 const fs = require('fs');
 const inquirer = require('inquirer');
-// TODO: Create an array of questions for user input
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
+//an array of questions for user input
 const questions = [
     {
-      type: 'input',
-    name: 'projectName',
-    message: 'What is your project name?'
+        type: "input",
+        name: "github",
+        message: "what is your github username?"
     },
     {
-        type:'input',
-        name: 'description',
-        message: 'Describe your project.'
-    }
+        type: "input",
+        name: "email",
+        message: "what is your email?"
+    },
+    {
+      type: "input",
+    name: "title",
+    message: "What is your project name?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Describe your project."
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "What license does your project have?",
+        choices: ["APACHE2.0", "Boost1.0", "GPL3.0", "BSD2" ,"BSD3", "None"]
+    },
+    {
+        type: "input",
+        name: "dependencies",
+        message: "Did you have any dependencies to install?",
+        default: "npm i"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What is the usage of this repo?"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "what command do you want running the tests?",
+        default: "npm test"
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Who are the contributors of this repo?"  
+    },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err => err ? console.log(err) : console.log('Readme Generated'));
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
     inquirer    
         .prompt(questions)
         .then(answers => {
+            console.log("Generating answers now...");
+            writeToFile("./dist/README.md", generateMarkdown({...inquirerAnswers }));
             //do stuff with the answers
             console.log(answers)
 
@@ -32,5 +74,6 @@ function init() {}
             //TODO - build a string that uses our answers to generate some markdown and write that to 
             //our file as the 'data' parameter. (replace the test values below)
         });
+    };
 // Function call to initialize app
 init();
